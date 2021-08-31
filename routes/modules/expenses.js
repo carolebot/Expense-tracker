ade4c546c6c453db83d4e27f0ce868730ab6dc09
carolebot <carol.tang79@gmail.com>
2021-08-31 17:44:32 +0800

    feat: add edit function

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -13,15 +13,37 @@ router.post('/', (req, res) => {
   const expense = new Expense({
     name, date, category, amount
   })
-  const formErrors = []
-  if (isNaN(Number(amount))) formErrors.push({ message: '金額欄位僅能輸入數字資料' })
+  // const formErrors = []
+  // if (isNaN(Number(amount))) formErrors.push({ message: '金額欄位僅能輸入數字資料' })
   
   console.log(expense)
   expense.save()
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  Expense.findById(id)
+    .lean()
+    .then((expense) => res.render('edit', { expense }))
+    .catch(error => console.log(error))
+})
 
 
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, date, category, amount } = req.body
+  Expense.findById(id)
+    .then(expense => {
+      expense.name = name,
+        expense.date = date,
+        expense.category = category,
+        expense.amount = amount,
+        expense.save()
+    })
+    .then(() => res.redirect('/'))    
+    .catch(error => console.log(error))
 })
 
 
